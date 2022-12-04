2c2a38f86b87dcb57c5d11f0995b2f4a4f1ce16b
Tom <thomas@irgang.eu>
2022-12-04 13:55:15 +0100

    impl tags, cleanup code

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Maillist
+# Mail-list
 
-Very simple mailing list tool.
+This script implements a simple "mail-list" tool.
+
+It is monitoring a given mailbox, using IMAP, and maintains
+a list of "subscribers". If a new mail is received, the tool
+creates a new mail, using the sender data provided, and sends
+this mail to all subscribers using SMTP.
--- mail_list.py
+++ mail_list.py
@@ -1,5 +1,12 @@
 """
-Simple maillist.
+Simple mail-list.
+
+This script implements a simple "mail-list" tool.
+
+It is monitoring a given mailbox, using imap, and maintains
+a list of "subscribers". If a new mail is received, the tool
+creates a new mail, using the sender data provided, and sends
+this mail to all subscribers using SMTP.
 """
 
 import argparse
@@ -17,6 +24,49 @@ from email.mime.base import MIMEBase
 from email import encoders
 from imap_tools import MailBox, AND, MailMessageFlags
 from dotenv import load_dotenv
+
+
+class Attachment:
+    """
+    Attachment data type.
+
+    Attachment is a internal data type to group
+    the information for one attachment. It is used
+    to transfer the attachments form IMAP to SMTP.
+    """
+    filename: str = None
+    mimetype: str = "application/octet-stream"
+    data: bytes = None
+
+
+class Message:
+    """
+    Message data type.
+
+    Message is a internal data type to group
+    the information needed for a mail which shall be sent
+    using SMTP. It is used for mail forwarding as well as
+    replies.
+    """
+    sender_name: str = ""
+    receivers: list[str] = []
+    subject: str = ""
+    text: str = ""
+    html: str = ""
+    attachments: list[Attachment] = []
+
+
+class SubscriberCheckResult:
+    """
+    Data type for communication between Subscribers and Receiver.
+
+    SubscriberCheckResult is a internal data type to group
+    the information needed for handling a new message in the
+    mailbox.
+    """
+    forward: bool = False
+    receivers: list[str] = []
+    unsubscribe_tag: str = '$>unsubscribe'
 
 
 class Config:
@@ -48,20 +98,28 @@ class Config:
                             help='send test mail on startup')
         parser.add_argument('-d', '--damon', action="store_true",
                             help='run as damon')
+        parser.add_argument('-r', '--reduce_logs', action="store_true",
+                            help='log only errors')
 
         args = parser.parse_args()
 
+        log_level = logging.INFO
+        if args.verbose:
+            log_level = logging.DEBUG
+        if args.reduce_logs:
+            log_level = logging.ERROR
         logging.basicConfig(filename=args.logfile,
-                            encoding='utf-8', level=logging.WARN)
+                            encoding='utf-8', level=log_level)
 
-        self.verbose = args.verbose
-        if self.verbose:
+        if not args.reduce_logs:
             logging.getLogger().addHandler(logging.StreamHandler())
+
+        if log_level == logging.DEBUG:
             logging.getLogger().setLevel(logging.DEBUG)
             logging.debug('verbose mode enabled')
 
-        self.damon = args.damon
-        logging.info('running as damon: %r', self.damon)
+        self.daemon = args.daemon
+        logging.info('running as daemon: %r', self.daemon)
 
         self.sleep = args.sleep
         logging.info('sleep time set to %i seconds', self.sleep)
@@ -92,6 +150,9 @@ class Config:
             mailbox = config['mailbox']
             self.mailbox_server = mailbox.get('server', None)
             self.mailbox_user = mailbox.get('user', None)
+        else:
+            self.mailbox_server = None
+            self.mailbox_user = None
 
         logging.debug('mailbox server: %s', self.mailbox_server)
         logging.debug('mailbox user: %s', self.mailbox_user)
@@ -103,6 +164,11 @@ class Config:
             self.smtp_port = smtp.get('port', '587')
             tls = smtp.get('tls', 'true')
             self.smtp_tls = tls.lower() == 'true'
+        else:
+            self.smtp_server = None
+            self.smtp_user = None
+            self.smtp_port = '587'
+            self.smtp_tls = True
 
         logging.debug('smtp server: %s', self.smtp_server)
         logging.debug('smtp user: %s', self.smtp_user)
@@ -113,15 +179,84 @@ class Config:
             sender = config['sender']
             self.sender_address = sender.get('address', None)
             self.sender_name = sender.get('name', None)
+        else:
+            self.sender_address = None
+            self.sender_name = None
 
         logging.debug('sender address: %s', self.sender_address)
         logging.debug('sender name: %s', self.sender_name)
 
-        self.test_receiver = config['test']['receiver']
+        if 'test' in config:
+            self.test_receiver = config['test'].get('receiver', None)
+        else:
+            self.test_receiver = None
 
         logging.debug('test receiver: %s', self.test_receiver)
 
+        if 'snippets' in config:
+            snippets = config['snippets']
+            self.list_name = snippets.get('list_name', self.sender_address)
+            self.subscribe_subject = snippets.get(
+                'subscribe_subject', 'Welcome!')
+            self.unsubscribe_subject = snippets.get(
+                'unsubscribe_subject', 'Bye!')
+
+            footer_text_file = snippets.get('footer_text', None)
+            if footer_text_file is not None and exists(footer_text_file):
+                with open(footer_text_file, 'r', encoding='utf-8') as f:
+                    self.footer_text = f.read()
+            else:
+                self.footer_text = ""
+
+            footer_html_file = snippets.get('footer_html', None)
+            if footer_html_file is not None and exists(footer_html_file):
+                with open(footer_html_file, 'r', encoding='utf-8') as f:
+                    self.footer_html = f.read()
+            else:
+                self.footer_html = ""
+
+            subscribe_text_file = snippets.get('footer_text', None)
+            if subscribe_text_file is not None and exists(subscribe_text_file):
+                with open(subscribe_text_file, 'r', encoding='utf-8') as f:
+                    self.subscribe_text = f.read()
+            else:
+                self.subscribe_text = ""
+
+            subscribe_html_file = snippets.get('footer_html', None)
+            if subscribe_html_file is not None and exists(subscribe_html_file):
+                with open(subscribe_html_file, 'r', encoding='utf-8') as f:
+                    self.subscribe_html = f.read()
+            else:
+                self.subscribe_html = ""
+
+            unsubscribe_text_file = snippets.get('footer_text', None)
+            if unsubscribe_text_file is not None and exists(unsubscribe_text_file):
+                with open(unsubscribe_text_file, 'r', encoding='utf-8') as f:
+                    self.unsubscribe_text = f.read()
+            else:
+                self.unsubscribe_text = ""
+
+            unsubscribe_html_file = snippets.get('footer_html', None)
+            if unsubscribe_html_file is not None and exists(unsubscribe_html_file):
+                with open(unsubscribe_html_file, 'r', encoding='utf-8') as f:
+                    self.unsubscribe_html = f.read()
+            else:
+                self.unsubscribe_html = ""
+
+        logging.debug('mail-list name: %s', self.list_name)
+        logging.debug('footer text: %s', self.footer_text)
+        logging.debug('footer html: %s', self.footer_html)
+        logging.debug('subscribe subject: %s', self.subscribe_subject)
+        logging.debug('subscribe text: %s', self.subscribe_text)
+        logging.debug('subscribe html: %s', self.subscribe_html)
+        logging.debug('unsubscribe subject: %s', self.unsubscribe_subject)
+        logging.debug('unsubscribe text: %s', self.unsubscribe_text)
+        logging.debug('unsubscribe html: %s', self.unsubscribe_html)
+
     def _get_secrets(self):
+        """
+        Read secrets from .env
+        """
         load_dotenv()
         self.mailbox_password = os.environ.get('mailbox_password')
         if len(self.mailbox_password) == 0:
@@ -140,34 +275,13 @@ class Config:
         assert self.sender_address is not None
         if self.send_test_mail:
             assert self.test_receiver is not None
-        if self.damon:
+        if self.daemon:
             assert self.sleep > 0
-
-
-class Attachment:
-    """
-    Attachment data type.
-    """
-    filename: str = None
-    mimetype: str = "application/octet-stream"
-    data: bytes = None
-
-
-class Message:
-    """
-    Message data type.
-    """
-    sender_name: str = ""
-    receivers: list[str] = []
-    subject: str = ""
-    text: str = ""
-    html: str = ""
-    attachments: list[Attachment] = []
 
 
 class Sender:
     """
-    The sender takes care of sending the mails to the subscribers.
+    The sender takes care of sending the mails.
     """
 
     def __init__(self, config: Config):
@@ -242,9 +356,9 @@ class Subscribers:
         """
         if exists(self.config.maillist_file):
             with open(self.config.maillist_file, 'r', encoding='utf-8') as file:
-                self.list = json.load(file)
+                self._list = json.load(file)
         else:
-            self.list = {'subscribers': []}
+            self._list = {'subscribers': []}
             self._save_list()
 
     def _save_list(self):
@@ -252,59 +366,129 @@ class Subscribers:
         Save the maillist as JSON file.
         """
         with open(self.config.maillist_file, 'w', encoding='utf-8') as file:
-            json.dump(self.list, file)
+            json.dump(self._list, file)
 
-    def _is_allowed(self, sender: str):
-        return sender in self.list['subscribers']
-
-    def get_receivers(self, subject: str, sender: str) -> tuple[bool, list[str]]:
+    def _get_key(self, tags: list[str] = None) -> str:
         """
-        Get all receivers for the given subject and sender.
-        """
-        forward = self._handle_command(subject, sender)
+        Get key calculates a unique key for the given tags.
 
-        if forward and self._is_allowed(sender):
-            receivers = self.list['subscribers'].copy()
+        If the list is empty, the default key is 'subscribers'.
+        """
+        if tags is None:
+            return 'subscribers'
+
+        lower_tags = [tag.lower() for tag in tags]
+        lower_tags.sort()
+        return '__'.join(lower_tags)
+
+    def _get_subscribers(self, tags: list[str] = None) -> list[str]:
+        tag_subscribers = self._list.get(self._get_key(tags), [])
+        full_subscribers = self._list.get('subscribers', [])
+        subscribers = tag_subscribers + full_subscribers
+        # remove duplicates
+        return list(set(subscribers))
+
+    def _is_allowed(self, sender: str, tags: list[str] = None) -> bool:
+        subscribers = self._get_subscribers(tags)
+        return sender in subscribers
+
+    def _get_tags(self, subject: str) -> list[str]:
+        tags = []
+        for part in subject.strip().split(' '):
+            part = part.strip()
+            if part.startswith('#') and len(part) > 1:
+                tag = part[1:]
+                tags.append(tag)
+
+        if len(tags) == 0:
+            return None
+        return tags
+
+    def check(self, subject: str, sender: str) -> SubscriberCheckResult:
+        """
+        Check if mail is valid, process commands, and get
+        all receivers for the given subject and sender.
+        """
+        tags = self._get_tags(subject)
+        forward = self._handle_command(subject, sender, tags)
+
+        if not forward:
+            return SubscriberCheckResult()
+
+        if self._is_allowed(sender, tags):
+            receivers = self._get_subscribers(tags).copy()
             receivers.remove(sender)
-            return (True, receivers)
-        else:
-            return (False, [])
 
-    def _handle_command(self, subject: str, sender: str) -> bool:
+            result = SubscriberCheckResult()
+            result.receivers = receivers
+            result.forward = True
+            return result
+        else:
+            logging.warning(
+                'sender %s tried to send %s, but is no subscriber', sender, subject)
+
+    def _handle_command(self, subject: str, sender: str, tags: list[str] = None) -> bool:
+        """
+        Process subscribe and unsubscribe commands.
+        """
         forward = True
         sub = subject.strip()
         if sub.startswith('$>'):
+            # do not forward command messages
             forward = False
-            command = sub[2:].split(' ')[0]
+            command = sub[2:]
             if command.lower().startswith('subscribe'):
-                self._add_subscriber(sender)
+                self._add_subscriber(sender, tags)
             elif command.lower().startswith('unsubscribe'):
-                self._remove_subscriber(sender)
+                self._remove_subscriber(sender, tags)
 
         return forward
 
-    def _add_subscriber(self, address):
+    def _add_subscriber(self, address, tags: list[str] = None):
+        """
+        Add a new subscriber for the given tags.
+
+        If tags are None, all messages are forwarded to the subscriber.
+        """
         logging.info('New subscriber: %s', address)
 
-        self.list['subscribers'].append(address)
-        self._save_list()
+        key = self._get_key(tags)
+        if address not in self._list[key]:
+            self._list[key].append(address)
+            self._save_list()
+
+        logging.info('new subscriber list for %r: %r', tags, self._list[key])
 
         message = Message()
-        message.subject = 'Welcome!'
-        message.text = 'Hi!\nYou successfully subscribed to this maillist.'
+        message.subject = self.config.subscribe_subject
+        message.text = self.config.subscribe_text % {
+            'list_name': self.config.list_name}
+        message.text = self.config.subscribe_html % {
+            'list_name': self.config.list_name}
         message.receivers = [address]
 
         self.sender.send_mail(message)
 
-    def _remove_subscriber(self, address):
+    def _remove_subscriber(self, address, tags: list[str] = None):
+        """
+        Remove a subscriber for the given tags.
+
+        If tags are None, the subscriber is removed from the global list.
+        """
         logging.info('User canceled subscription: %s', address)
 
-        self.list['subscribers'].remove(address)
-        self._save_list()
+        key = self._get_key(tags)
+        if address in self._list[key]:
+            self._list[key].remove(address)
+            self._save_list()
+
+        logging.info('new subscriber list for %r: %r', tags, self._list[key])
 
         message = Message()
-        message.subject = 'Bye!'
-        message.text = 'Hi!\nYou successfully canceled your subscription to this maillist.'
+        message.text = self.config.unsubscribe_text % {
+            'list_name': self.config.list_name}
+        message.text = self.config.unsubscribe_html % {
+            'list_name': self.config.list_name}
         message.receivers = [address]
 
         self.sender.send_mail(message)
@@ -324,7 +508,6 @@ class Receiver:
         """
         Fetch and process all new mails.
         """
-
         logging.info("Processing new messages ...")
 
         with MailBox(self.config.mailbox_server).login(
@@ -346,12 +529,12 @@ class Receiver:
         self._log_message(msg)
 
         subject = msg.subject
-        forward, receivers = self.subscribers.get_receivers(subject, msg.from_)
-        if not forward:
+        result = self.subscribers.check(subject, msg.from_)
+        if not result.forward:
             logging.debug('message shall be not forwarded')
             return
 
-        if len(receivers) == 0:
+        if len(result.receivers) == 0:
             logging.info('no subscribers for %s', subject)
             return
 
@@ -359,7 +542,7 @@ class Receiver:
         message.subject = subject
         message.text = msg.text
         message.html = msg.html
-        message.receivers += receivers
+        message.receivers += result.receivers
         message.sender_name = msg.from_values.name
 
         for att in msg.attachments:
@@ -372,6 +555,9 @@ class Receiver:
         self.sender.send_mail(message)
 
     def _log_message(self, msg):
+        """
+        Log the given message
+        """
         logging.info('From: %s', msg.from_)
         logging.debug('To: %s', msg.to)
         logging.info('Subject: %s', msg.subject)
@@ -438,7 +624,7 @@ def main():
 
     maillist = Maillist(config)
 
-    if config.damon:
+    if config.daemon:
         while True:
             maillist.process_mails()
             maillist.sleep()
